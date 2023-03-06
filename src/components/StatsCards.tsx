d27c34170521c4d391237c85f308357bf87f255f
ClaytonDewey <clay@dryadmedia.com>
2023-03-06 14:10:52 -0600

    Pass needed props to Card component

--- src/components/StatsCards.tsx
+++ src/components/StatsCards.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import data from '../data.json';
+import Card from './Card';
 
 type StatsCardsProps = {};
 
@@ -7,7 +8,12 @@ const StatsCards: React.FC<StatsCardsProps> = () => {
   return (
     <>
       {data.map((stat, index) => (
-        <div key={index}>{stat.title}</div>
+        <Card
+          key={index}
+          title={stat.title}
+          timeframes={stat.timeframes}
+          statsBy='weekly'
+        />
       ))}
     </>
   );
