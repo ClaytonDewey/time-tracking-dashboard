44f4a8c3996b1d9394c0e8cc7d03390e646faf74
Clayton Dewey <clay@dryadmedia.com>
2023-03-07 18:13:42 -0600

    Update container styles

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,6 +7,11 @@ type ContainerProps = {};
 
 const StyledContainer = styled.main`
   width: 100%;
+
+  @media screen and (min-width: 48em) {
+    display: flex;
+    gap: 3rem;
+  }
 `;
 
 const Container: React.FC<ContainerProps> = () => {
