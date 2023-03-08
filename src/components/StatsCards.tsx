dbae08f7822af334c2bb0edf40c416aebeee45d2
Clayton Dewey <clay@dryadmedia.com>
2023-03-07 18:14:00 -0600

    Wrap cards in flex container

--- src/components/StatsCards.tsx
+++ src/components/StatsCards.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import data from '../data.json';
 import Card from './Card';
 
 type StatsCardsProps = {};
 
+const FlexGrid = styled.div`
+  display: flex;
+  gap: 3rem;
+  flex-flow: row wrap;
+`;
+
 const StatsCards: React.FC<StatsCardsProps> = () => {
   return (
-    <>
+    <FlexGrid>
       {data.map((stat, index) => (
         <Card
           key={index}
@@ -15,7 +22,7 @@ const StatsCards: React.FC<StatsCardsProps> = () => {
           statsBy='weekly'
         />
       ))}
-    </>
+    </FlexGrid>
   );
 };
 export default StatsCards;
